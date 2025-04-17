b6f226d8e2ad021eb0d7a075bd91dd9c25856b47
Kelly Hum <kellyhum88@gmail.com>
2025-04-17 12:14:43 -0700

    add practice route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes.wordbank_routes import wordbank_blueprint
+from app.routes.practice_routes import practice_blueprint
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
     app.register_blueprint(wordbank_blueprint, url_prefix="/wordbank")
+    app.register_blueprint(practice_blueprint, url_prefix="/practice")
 
     return app
